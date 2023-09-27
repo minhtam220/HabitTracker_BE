0e906ccec0ad23666ef1f072462148f0b6e5004c
minhtam220 <minhtam220@gmail.com>
2023-09-27 19:36:57 +0700

    open cors to all websites

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
